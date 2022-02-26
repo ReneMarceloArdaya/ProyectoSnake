1807789f275b48dd878a0945b0d9306dc2f225b4
ReneMarceloArdaya <sc.rene.oruno.a@upds.net.bo>
2022-02-26 14:50:53 -0400

    errores ortograficos coregidos

--- snake.py
+++ snake.py
@@ -12,9 +12,9 @@ ALTO = 480
 
 #imagen de la manzana  
 manzanaD = pygame.transform.scale(pygame.image.load(os.path.join(r"man.png")),(20,20))
-
-
 pantalla = pygame.display.set_mode((ANCHO,ALTO))
+
+
 #tamaño de la letra 
 texto_vida = pygame.font.SysFont("Russo One ",20)
 texto_puntaje = pygame.font.SysFont("Russo One",20)
@@ -22,7 +22,7 @@ texto_puntaje = pygame.font.SysFont("Russo One",20)
 
 
 
-#clase de la vivirita
+#clase de la Snake
 class Snake:
 
     #constructor
@@ -31,7 +31,7 @@ class Snake:
 		self.direction = Vector2(0,-20)
 		self.add = False
 
-    #dibujo de la vivorita
+    #dibujo de la Snake
 	def dibujo(self):
 		for bloque in self.body:
 			pygame.draw.rect(pantalla,(0,0,255),(bloque.x,bloque.y,10,10))
@@ -41,7 +41,7 @@ class Snake:
 	def mover(self):
 		
 		#[0,1,2] --> [0,1] --> [None,0,1] --> [-1,0,1]
-        #crece la vivorita o incrementa el tamaño
+        #crece la Snake o incrementa el tamaño
 		if self.add == True:
 			body_copy = self.body
 			body_copy.insert(0,body_copy[0]+self.direction)
@@ -52,7 +52,7 @@ class Snake:
 			body_copy.insert(0,body_copy[0]+self.direction)
 			self.body = body_copy[:]
 
-    #funciones de mover a la vivirita
+    #funciones de mover a la Snake
 	def mover_arriba(self):
 		self.direction = Vector2(0,-20)
 
@@ -65,29 +65,23 @@ class Snake:
 	def mover_izquierda(self):
 		self.direction = Vector2(-20,0)
 
-    #funcionde colision de la vivorita con los bordes
+    #funcione colisión de la snake con los bordes
 	def colision(self):
 		if self.body[0].x >= ANCHO+10 or self.body[0].y >= ALTO+10 or self.body[0].x <= -10 or self.body[0].y <= -10:
 			return True
 
-		#colision  si se toca asi misma
+		#colisión  si se toca asi misma
 		for i in self.body[1:]:
 			if self.body[0] == i:
 				return True
     
-
-
-
-
-
-
 
 #clase de la manzana
 class Manzana:
 	def __init__(self):
 		self.generate()
 
-    #funcion de la manzanita
+    #funcion de la manzana
 	def dibujarM(self):
 		pantalla.blit(manzanaD,(self.pos.x,self.pos.y))
 
@@ -97,7 +91,7 @@ class Manzana:
 		self.y = random.randrange(0,ALTO/20)
 		self.pos = Vector2(self.x*20,self.y*20)
 
-    #funcion donde conecta la vivorita con la manzana
+    #funcion donde conecta la Snake con la manzana
 	def come_comida(self,snake):
 
 		if snake.body[0] == self.pos:
@@ -114,9 +108,7 @@ class Manzana:
 		return False
 
 
-
-
-#funcion de ejecusion
+#funcion de ejecución
 def main():
 
     #llama a las clase
@@ -125,19 +117,20 @@ def main():
 	puntos = 0
 	vida=3
 
-    #velosidad de la vivorita
+    #velocidad de la vivorita
 	fps = pygame.time.Clock()
 
 	while True:
-        #velsidad
+        #veleidad
 		fps.tick(5)
 
 		for event in pygame.event.get():
+
             #funcion de la ventana de salir
 			if event.type == pygame.QUIT:
 				quit()
             
-            #funcionde los teclado 
+            #funcion de los teclado 
 
 			if event.type == pygame.KEYDOWN and snake.direction.y != 20:
 				if event.key == pygame.K_UP:
@@ -170,20 +163,18 @@ def main():
 		if manzana.come_comida(snake):
 			puntos+=1
 			
-        #donde se ejecuta colision
+        #donde se ejecuta colisión
 		snake.colision()
 		if snake.colision():
 			vida -=1
 
-			
-
-        #muestra la pocision del puntaje en la pantalla
+        #muestra la posición del puntaje en la pantalla
 
 		texto_v= texto_vida.render("vida : "+ str (vida),True, (255,255, 255))
 
 		pantalla.blit(texto_v,(10,10))
 		
-		text = texto_puntaje.render("puntuacion: {}".format(puntos),1,(255,255,255))
+		text = texto_puntaje.render("puntuación: {}".format(puntos),1,(255,255,255))
 		pantalla.blit(text,(ANCHO-text.get_width()-20,20))
 
 		pygame.display.update()
